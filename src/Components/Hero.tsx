67d5b7df4f70f66558ca4a5f6b2cc93a421484f7
Carlos Eduardo Drago <carlosemdl@gmail.com>
2025-09-21 00:38:57 -0300

    1.0.0

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -4,14 +4,12 @@ import Socials from './Socials';
 
 export default function Hero({children}: React.PropsWithChildren){
     return (
-        <div className="bg-amber-300 flex flex-col justify-center items-center w-1/2">
+        <div className=" flex flex-col justify-center w-screen text" style={{backgroundColor: "#212A31", color: "#D3D9D4"}}>
             <div className='flex flex-row items-center'>
-                <div className='hover:-translate-2 duration-250'>
-                    <img src="/support image 1.png" className='absolute w-60 z-0 pr-5 pt-7' alt="Composição com a Foto" />
-                    <img src="/favicon.png" className='relative w-60 z-1 pr-10' alt="Foto"/>
-                </div>
                 <div>
-                    <h2 className="font-mono text-3xl">Carlos Lopes</h2>
+                    <h2 className="font-bold text-8xl">Carlos Lopes</h2>
+                    <p className="text-2xl pt-4">Desenvolvedor Web Full-Stack com foco em React, Node.JS e .NET</p>
+                    
                     <Socials/>
                     
                 </div>
